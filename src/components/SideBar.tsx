d32f011d0186de71ad969af736d9da08b01fd76a
Alan Guimarães Llantada <alan@redeimagem.com.br>
2021-08-21 12:14:38 -0300

    Challange 02 - implementation

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,49 @@
-export function SideBar() {
-  // Complete aqui
+import { useState, useEffect } from "react";
+
+import { Button } from './Button';
+
+import { api } from "../services/api";
+
+import '../styles/sidebar.scss';
+
+interface SideBarProps {
+  handleSelectedGenreId : Function
+  selectedGenreId: number
+}
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+export function SideBar(props: SideBarProps) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  function handleClickButton(id: number) {
+    props.handleSelectedGenreId(id);
+  }
+  
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={props.selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
